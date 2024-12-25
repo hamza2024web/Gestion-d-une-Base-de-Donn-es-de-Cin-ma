81691724a6d4d8c618930e0f7092806ddfabd269
hamza2024web <hamzaakroubi1528@gmail.com>
2024-12-25 18:04:37 +0100

    l'utilisatioin de docker dans cette projet

---
+++ project/index.php
@@ -0,0 +1,12 @@
+<?php
+class personne {
+     private $nom;
+     public function hello (){
+          echo 'Never give up !
+          win the war with your mind hamza!';
+     }
+}
+
+$p1 = new personne();
+$p1 -> hello();
+?>
